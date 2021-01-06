6b931e4e5378608f26bdc7b9d93edb6c460a97fc
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-05 23:58:55 -0300

    Added Employee class with its properties

---
+++ src/Generics/Collections/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public int DepartmentId { get; set; }
+    }
+}
