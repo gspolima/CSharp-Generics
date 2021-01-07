ee7e7bb51bf68c80145dfd56a9756ee6769ee8ff
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-06 22:23:26 -0300

    Clearing Main code

--- src/Generics/Collections/Program.cs
+++ src/Generics/Collections/Program.cs
@@ -7,19 +7,7 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            var numbers = new List<int>();
-            var listCapacity = -1;
-
-            // don't hit Crtl F5
-            while (true)
-            {
-                if (numbers.Capacity != listCapacity)
-                {
-                    listCapacity = numbers.Capacity;
-                    Console.WriteLine(listCapacity);
-                }
-                numbers.Add(1);
-            }
+            
         }
     }
 }
