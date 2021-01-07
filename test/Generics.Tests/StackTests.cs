d4488c33d98994b28705ba7b1ffe6b8e86f210ee
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-06 22:50:42 -0300

    Added tests that prove some of Stack<T> functionalities.

---
+++ test/Generics.Tests/StackTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Generics.Tests
+{
+    public class StackTests
+    {
+        [Fact]
+        public void Can_Peek_At_Next_Item()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(70);
+            stack.Push(14);
+            stack.Push(55);
+
+            var expected = 55;
+            var actual = stack.Pop();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Can_Search_With_Contains()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(70);
+            stack.Push(14);
+            stack.Push(55);
+
+            Assert.True(stack.Contains(70));
+        }
+
+        [Fact]
+        public void Can_Convert_To_Array()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(70);
+            stack.Push(14);
+            stack.Push(55);
+
+            var expected = 70;
+            var stackAsArray = stack.ToArray();
+            Assert.Equal(expected, stackAsArray[2]);
+        }
+    }
+}
