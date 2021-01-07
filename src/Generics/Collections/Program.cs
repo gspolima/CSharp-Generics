69ee538b608f9cc88b78a1057ef733538d4b6a6f
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-06 22:49:43 -0300

    Stacks store elements in a reversed order or LIFO

--- src/Generics/Collections/Program.cs
+++ src/Generics/Collections/Program.cs
@@ -7,7 +7,29 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            
+            var queue = new Queue<Employee>();
+            queue.Enqueue(new Employee() { Name = "Ash" });
+            queue.Enqueue(new Employee() { Name = "Judy" });
+            queue.Enqueue(new Employee() { Name = "Joe" });
+
+            while(queue.Count > 0 )
+            {
+                var employee = queue.Dequeue();
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.WriteLine("----------");
+
+            var stack = new Stack<Employee>();
+            stack.Push(new Employee() { Name = "Ash" });
+            stack.Push(new Employee() { Name = "Judy" });
+            stack.Push(new Employee() { Name = "Joe" });
+
+            while (stack.Count > 0)
+            {
+                var employee = stack.Pop();
+                Console.WriteLine(employee.Name);
+            }
         }
     }
 }
