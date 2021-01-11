6e7fb4a8f6ecf897c7497a5c0c56cc9733d1334f
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-11 00:14:02 -0300

    Added new project and starting code for the Circular Buffer

---
+++ src/Generics/DataStructures/DataStructures/CircularBuffer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class CircularBuffer
+    {
+        private double[] _buffer;
+        private int _start;
+        private int _end;
+
+        public CircularBuffer() : this(capacity: 10)
+        {
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            this._buffer = new double[capacity + 1];
+            this._start = 0;
+            this._end = 0;
+        }
+    }
+}
---
+++ test/DataStructures.Tests/DataStructures.Tests/CircularBufferTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class CircularBufferTests
+    {
+        [Fact]
+        public void Passing_Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
