7b07d69e32602fed51f16b1a9a6e4d90531f3411
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-11 23:09:18 -0300

    Additional starting code and underscores removed before variable names.

--- src/Generics/DataStructures/CircularBuffer.cs
+++ src/Generics/DataStructures/CircularBuffer.cs
@@ -4,11 +4,11 @@ using System.Text;
 
 namespace DataStructures
 {
-    class CircularBuffer
+    class CircularBuffer<T>
     {
-        private double[] _buffer;
-        private int _start;
-        private int _end;
+        private T[] buffer;
+        private int start;
+        private int end;
 
         public CircularBuffer() : this(capacity: 10)
         {
@@ -16,9 +16,40 @@ namespace DataStructures
 
         public CircularBuffer(int capacity)
         {
-            this._buffer = new double[capacity + 1];
-            this._start = 0;
-            this._end = 0;
+            this.buffer = new T[capacity + 1];
+            this.start = 0;
+            this.end = 0;
+        }
+        public void Write(T value)
+        {
+            buffer[end] = value;
+            end = (end + 1) % buffer.Length;
+            if (end == start)
+            {
+                start = (start + 1) % buffer.Length;
+            }
+        }
+
+        public T Read()
+        {
+            var result = buffer[start];
+            start = (start + 1) % buffer.Length;
+            return result;
+        }
+
+        public int Capacity
+        {
+            get { return buffer.Length; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return end == start; }
+        }
+
+        public bool IsFull
+        {
+            get { return (end + 1) % buffer.Length == start; }
         }
     }
 }
