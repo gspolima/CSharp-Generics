e0a769ead2570b8c41b1ab5e035c34d9c45d632a
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-11 23:47:50 -0300

    Added IBuffer<T> as a higher layer of abstraction.

---
+++ src/Generics/DataStructures/IBuffer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    interface IBuffer<T>
+    {
+        bool IsEmpty { get; }
+
+        void Write(T value);
+
+        T Read();
+    }
+}
