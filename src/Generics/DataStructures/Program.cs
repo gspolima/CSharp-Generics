140beebc63a314d20d62fb5942ccc226ef141fd0
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-11 23:50:38 -0300

    Added methods for processing user input and data inside the generic buffer.

--- src/Generics/DataStructures/Program.cs
+++ src/Generics/DataStructures/Program.cs
@@ -6,7 +6,37 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Data Structures!");
+            var circularBuffer = new CircularBuffer<double>();
+
+            ProcessInput(circularBuffer);
+            ProcessBuffer(circularBuffer);
+        }
+
+        private static void ProcessBuffer(IBuffer<double> buffer)
+        {
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ProcessInput(IBuffer<double> buffer)
+        {
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
         }
     }
 }
