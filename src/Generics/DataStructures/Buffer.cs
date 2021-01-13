f65f353d0aa1bbe016fa8c01294885c91526afa2
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-12 23:41:26 -0300

    Added Buffer<T> as a derivative of IBuffer<T>, implementing a generic queue.

---
+++ src/Generics/DataStructures/Buffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class Buffer<T> : IBuffer<T>
+    {
+        Queue<T> queue = new Queue<T>();
+
+        public bool IsEmpty => queue.Count == 0;
+
+        public T Read()
+        {
+            return queue.Dequeue();
+        }
+
+        public void Write(T value)
+        {
+            queue.Enqueue(value);
+        }
+    }
+}
