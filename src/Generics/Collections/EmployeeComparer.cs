57fda302040cd0a27e7644579a53920f782eafb8
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-13 23:49:47 -0300

    Added class to compare Employees for equality by their names.

---
+++ src/Generics/Collections/EmployeeComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Collections
+{
+    class EmployeeComparer : IEqualityComparer<Employee>, IComparer<Employee>
+    {
+        public int Compare([AllowNull] Employee x, [AllowNull] Employee y)
+        {
+            return string.Compare(x.Name, y.Name);
+        }
+
+        public bool Equals([AllowNull] Employee x, [AllowNull] Employee y)
+        {
+            return string.Equals(x.Name, y.Name);
+        }
+
+        public int GetHashCode([DisallowNull] Employee obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+}
