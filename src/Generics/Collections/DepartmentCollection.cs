a857423a5a976b76e5a16453f6b966f00606f259
Gustavo Sampaio <sampaioglima@gmail.com>
2021-01-14 23:01:31 -0300

    Defined class to abstract the implementation of SortedDictionary with a method that returns itself.

---
+++ src/Generics/Collections/DepartmentCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    class DepartmentCollection : SortedDictionary<string, SortedSet<Employee>>
+    {
+        public DepartmentCollection Add(string departmentName, Employee employee)
+        {
+            if (!ContainsKey(departmentName))
+                Add(departmentName, new SortedSet<Employee>(new EmployeeComparer()));
+
+            this[departmentName].Add(employee);
+            return this;
+        }
+    }
+}
